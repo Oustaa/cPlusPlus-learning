05db49ea7e19e75969b1e2f8b41f325d1b8b3a38
Oustaa <otailaba98@gmail.com>
2025-01-23 22:09:03 +0100

    Remove VSCode configuration files and refactor Player and Game classes for improved win-checking logic and gameplay flow

--- XO/src/Player.hpp
+++ XO/src/Player.hpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 #include "utils.hpp"
 
@@ -30,15 +31,32 @@ public:
 
     bool check_wins()
     {
+        int winning_paths[8][3] = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9},
+            {1, 4, 7},
+            {2, 5, 8},
+            {3, 6, 9},
+            {1, 5, 9},
+            {3, 5, 7},
+        };
 
-        system("clear");
-        vector<int>::iterator itr = played_spots.begin();
-
-        while (itr != played_spots.end())
+        for (const auto &path : winning_paths)
         {
-            cout << *itr << endl;
+            int match = 0;
+            for (auto spot : path)
+            {
+                auto it = find(played_spots.begin(), played_spots.end(), spot);
 
-            itr++;
+                if (it != played_spots.end())
+                {
+                    match++;
+                }
+            }
+
+            if (match == 3)
+                return true;
         }
 
         return false;
