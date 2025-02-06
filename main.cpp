d2fbf9993972c4b77432c3bad3b481d6702c7562
Oustaa <otailaba98@gmail.com>
2025-02-06 14:54:31 +0100

    Implement mergeAlternately function in main.cpp; enhance TCP server to handle multiple clients with threading

--- main.cpp
+++ main.cpp
@@ -7,13 +7,48 @@ using namespace std;
 
 constexpr int double_it(int arg) { return arg * arg; }
 
+string mergeAlternately(string word1, string word2)
+{
+    string merged, left_over = "";
+    int min_len, word1_len = word1.length(), word2_len = word2.length();
+
+    if (word1_len != word2_len)
+    {
+        min_len = min(word1.length(), word2.length());
+        left_over = word1_len > word2_len ? word1.substr(min_len, word1_len) : word2.substr(min_len, word2_len);
+    }
+    else
+    {
+        min_len = word1_len;
+    }
+
+    for (int i = 0; i < min_len; i++)
+    {
+        merged += word1[i];
+        merged += word2[i];
+    }
+
+    merged += left_over;
+
+    return merged;
+}
+
 int main()
 {
-    cout << SQRT(4) << endl;
-    cout << SQRT(4 + 1) << endl;
 
-    int bad_result = double_it(4 + 1);
+    string mergerd = mergeAlternately("Oussama", "1234");
+    cout << mergerd << endl;
 
-    cout << bad_result << endl;
     return 0;
+
+    // cout << SQRT(4) << endl;
+    // cout << SQRT(4 + 1) << endl;
+
+    // int bad_result = double_it(4 + 1);
+
+    // cout << bad_result << endl;
+    // return 0;
+    string name = "Oussama Tail";
+    int length = name.length();
+    cout << min(length, 15) << endl;
 }
